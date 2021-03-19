81fbfacb1fecdffd885fc06a0ae80587bec4978b
wangyong <yundan_wy@126.com>
2021-03-19 09:45:47 +0800

    #add:优化pom

--- little-hotel-web/src/main/java/com/kevin/little/hotel/api/login/service/impl/LoginServiceImpl.java
+++ little-hotel-web/src/main/java/com/kevin/little/hotel/api/login/service/impl/LoginServiceImpl.java
@@ -2,6 +2,7 @@ package com.kevin.little.hotel.api.login.service.impl;
 
 import com.kevin.little.hotel.api.login.dto.LoginRequestDTO;
 import com.kevin.little.hotel.api.login.service.LoginService;
+import com.ruyuan.little.project.redis.api.RedisApi;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginServiceImpl implements LoginService {
 
-//  @Reference(version = "1.0.0",interfaceClass = RedisApi.class,cluster = "failfast")
-//  private RedisApi redisApi;
+  @Reference(version = "1.0.0", interfaceClass = RedisApi.class, cluster = "failfast")
+  private RedisApi redisApi;
 
   @Override
   public void firstLoginDistributeCoupon(LoginRequestDTO loginRequestDTO) {
