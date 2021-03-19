8ede3afed95e334edbd68d2fb03ba205818f7c1c
wangyong <yundan_wy@126.com>
2021-03-19 09:38:12 +0800

    #add:更改jdk版本

--- little-hotel-web/src/main/java/com/kevin/little/hotel/api/login/service/impl/LoginServiceImpl.java
+++ little-hotel-web/src/main/java/com/kevin/little/hotel/api/login/service/impl/LoginServiceImpl.java
@@ -7,19 +7,21 @@ import org.springframework.stereotype.Service;
 
 /**
  * 登录服务具体实现
+ *
  * @author wangyong
  */
 @Service
 public class LoginServiceImpl implements LoginService {
 
-
 //  @Reference(version = "1.0.0",interfaceClass = RedisApi.class,cluster = "failfast")
 //  private RedisApi redisApi;
 
+  @Override
   public void firstLoginDistributeCoupon(LoginRequestDTO loginRequestDTO) {
 
   }
 
+  @Override
   public void resetFirstLoginStatus(LoginRequestDTO loginRequestDTO) {
 
   }
